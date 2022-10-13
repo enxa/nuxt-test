c95e5a71648983c940912ce1d1d1c30e4dc52f48
enxa <cezary.walczak.pl@gmail.com>
2022-10-13 05:14:38 +0200

    add public folder

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,5 @@
-<script setup>
-  const { data: posts, pending, refresh, error } = await useLazyFetch('https://jsonplaceholder.typicode.com/posts', {
-    pick: []
-  })
-
-  const first = useFirst()
-</script>
-
 <template>
   <section>
-    Posts:
-    <Nav />
-
-    <div v-if="pending">
-      Pending...
-    </div>
-
-    <div v-else>
-      <ul v-for="post in posts" :key="post.id">
-        <li>{{post.title}}</li>
-      </ul>
-    </div>
-
-    <div v-if="error">
-      {{error.message}}
-    </div>
-
-    <button @click="() => refresh()">Refresh</button>
-    <button @click="() => refreshNuxtData()">Refresh all</button>
-
-    <hr>
-
-    {{ first }}
-
-    <ContentDoc />
+    <NuxtWelcome />
   </section>
 </template>
